753f7aca79e19506d92448eb429003a20a9a931d
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-10-27 19:28:04 +0100

    add paintPixels funtion the container now adjusts to the size of the window

--- javascript.js
+++ javascript.js
@@ -1,17 +1,42 @@
 const button = document.querySelector('#btn');
-let questionGrids = 'prompt("What number of squares per side do you want for the grid?")';
+const container = document.querySelector('#container');
 
-button.setAttribute('onclick', questionGrids);
-console.log(questionGrids)
+let questionGrids;
 
-createGridDivs();
+button.addEventListener('click', () => {
+    container.innerHTML = '';
+    questionGrids = prompt("What number of squares per side do you want for the grid?");
+    console.log(questionGrids);
+    createGridDivs(questionGrids);
+    paintPixels()
+})
 
-function createGridDivs() {
-    let container = document.querySelector('#container');
 
-    for(i=0; i < 16; i++) {
-        let div = document.createElement('div');
-        div.textContent = 'hello World';
-        container.appendChild(div);
+function createGridDivs(cantSideDivs) {
+
+    container.style.cssText += `grid-template-columns: repeat(${cantSideDivs}, auto); grid-gap: 1px;`;
+
+    for(x=0; x < cantSideDivs; x++) {
+        for(y = 0; y < cantSideDivs; y++) {
+            let div = document.createElement('div');
+            //div.textContent = 'hello World';
+            //div.style.textAlign = 'center';
+            div.setAttribute('id', 'pixel')
+            //div.style.height = '100px';
+            div.style.borderRadius = '6px'
+            container.appendChild(div);
+        }
     }
+}
+
+function paintPixels() {
+    const divs = document.querySelectorAll('#pixel');
+    const divsArray = [...divs]
+
+    divsArray.forEach((pixel) => {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'black';
+            console.log('hola');
+        })
+    })
 }
