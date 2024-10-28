546c8383d7d3dfa09c3160555936719698f7d267
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-10-28 20:45:48 +0100

    changed the style of the page add function randomColor add cumulative opacity

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ let questionGrids;
 button.addEventListener('click', () => {
     container.innerHTML = '';
     questionGrids = prompt("What number of squares per side do you want for the grid?");
-    console.log(questionGrids);
+    questionGrids > 100 ? questionGrids = 100 : '';
     createGridDivs(questionGrids);
     paintPixels()
 })
@@ -35,8 +35,19 @@ function paintPixels() {
 
     divsArray.forEach((pixel) => {
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'black';
-            console.log('hola');
+            let actualOpacity = +pixel.style.opacity;
+            pixel.style.backgroundColor = randomcolor();
+            
+            actualOpacity < '1' ? pixel.style.opacity = actualOpacity + 0.1 : '';
         })
     })
+}
+
+function randomcolor() {
+    let hexChars = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hexChars[Math.floor(Math.random() * hexChars.length)];
+    }
+    return color;
 }
